64aa350ffa9925b92cb497a0918fdec921df93de
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-01-31 17:13:56 +0100

    Added nlog .NET sample

---
+++ examples/dotnet-nlog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+var loggerFactory = LoggerFactory.Create(builder => {
+	builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+	builder.AddNLog();
+});
+var logger = loggerFactory.CreateLogger("Program");
+
+logger.LogTrace("Sample");
+logger.LogDebug("Sample");
+logger.LogInformation("Sample");
+logger.LogWarning("Sample");
+logger.LogError("Sample");
+logger.LogCritical("Sample");
+
---
+++ examples/dotnet-nlog/README.md
@@ -0,0 +1,11 @@
+# Flog .NET sample using NLog
+
+## Running
+
+1. Install .NET SDK 5.0 (or higher)
+
+2. Run
+
+   ```sh
+   dotnet run | flog
+   ```
