4f837a5e1433db2299d17cd75c4c6b227f25691d
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-01-31 16:55:05 +0100

    Added .NET sample

---
+++ examples/dotnet/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+var loggerFactory = new LoggerFactory();
+loggerFactory.AddConsole();
+var logger = loggerFactory.CreateLogger<Program>();
+
+logger.LogTrace("Sample");
+logger.LogDebug("Sample");
+logger.LogInformation("Sample");
+logger.LogWarning("Sample");
+logger.LogError("Sample");
+logger.LogCritical("Sample");
+
