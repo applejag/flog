cd0369a8603662fa40915191226662446595c00a
Kalle Jillheden <kalle.jillheden@gmail.com>
2021-02-13 20:27:39 +0100

    Added license to files

--- internal/apex/handlers/console/console.go
+++ internal/apex/handlers/console/console.go
@@ -1,3 +1,19 @@
+// Filter multiline logs based on the log's severity
+// Copyright (C) 2021  Kalle Jillheden
+//
+// flog is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// flog is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 // Package console implements a development-friendly textual handler for apex.
 //
 // This was originally forked from
